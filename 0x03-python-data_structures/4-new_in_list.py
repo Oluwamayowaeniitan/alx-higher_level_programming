3c476f2a80ae6d23d92b57ca66fc924fafe644cb
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-01-10 07:24:03 -0800

    replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx >= len(my_list):
         return my_list
-    else:
-        my_list.replace(idx, element)
+    temp_list = list(my_list)
+    temp_list[idx] = element
+    return temp_list
