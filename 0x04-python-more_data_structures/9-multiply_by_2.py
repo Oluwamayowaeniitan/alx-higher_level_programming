92696e37c797d46e6514d0e3031963b1da886815
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-01-11 11:50:38 -0800

    multi

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        new = (a_dictionary(i) * 2)
+    new = {}
+    for k, v in a_dictionary.items():
+        new[k] = v * 2
         return new
