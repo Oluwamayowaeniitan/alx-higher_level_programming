0a8c277cd381bbd6d2178d31194a0020dfcb0b78
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-02-07 05:59:23 -0800

    tenth

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+    module
+'''
+
+
+class Student:
+    
+    def __init__(self, first_name, last_name, age):
+        '''
+            initialize instances
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+            retrieves information
+        '''
+        return self.__dict__
