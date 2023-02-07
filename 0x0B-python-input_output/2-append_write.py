5622975f43fce8a02cf3e3b7a4178cda31fe57ae
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-02-07 04:22:11 -0800

    third

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,9 +3,11 @@
     module to append
 '''
 
+
 def append_write(filename="", text=""):
     '''
         write an input
     '''
+
     with open(filename, "a", encoding="utf-8") as filetemp:
         return filetemp.write(text)
