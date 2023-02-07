412d00a78bfd93dfe3973f3fa5c9a57e3336196e
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-02-07 05:01:33 -0800

    seventh

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     '''
         json function
     '''
-    filename = json.dump(my_obj)
-    return filename
+    with open(filename, mode="w") as filetemp:
+        filetemp.write(json.dumps(my_obj))
