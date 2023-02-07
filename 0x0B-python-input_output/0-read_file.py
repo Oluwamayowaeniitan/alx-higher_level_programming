ad9ea48a1380d160acf552d0bf9603c8a38a0f6f
Oluwamayowaeniitan <oluwamayoweniitan@gmail.com>
2023-02-07 03:50:35 -0800

    first

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("filename", encoding="utf-8") as filetemp:
-        print(filetemp.read())
+    with open("filename", "r", encoding="utf-8") as filetemp:
+        read_data = filetemp.read()
+    print(read_data, end="")
